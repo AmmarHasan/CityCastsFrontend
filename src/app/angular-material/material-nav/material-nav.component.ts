9ad0c570e792899218dd5f7cf20bcf9692f2aa86
Ammar Hasan <hasanammar94@gmail.com>
2018-05-11 19:09:06 +0200

    Basic layout

--- src/app/angular-material/material-nav/material-nav.component.ts
+++ src/app/angular-material/material-nav/material-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 
@@ -7,7 +7,17 @@ import { Observable } from 'rxjs';
   templateUrl: './material-nav.component.html',
   styleUrls: ['./material-nav.component.css']
 })
-export class MaterialNavComponent {
-  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+export class MaterialNavComponent  {
+  mobileQuery: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  fillerNav = Array(50).fill(0).map((_, i) => `Nav Item ${i + 1}`);
+
+  fillerContent = Array(50).fill(0).map(() =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
 }
